ee036775ff2d36ded8bf92d912f97757f9a24043
Teo <teowenlong0316@gmail.com>
2022-02-13 14:32:34 +0800

    changes

--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://127.0.0.1:27017/blog-backend')
-mongoose.connect(process.env.MONGODB_URL)
+require('dotenv').config();
+
+const uri = process.env.MONGODB_URL
+mongoose.connect(uri, { useNewUrlParser: true })
   .then(() => console.log('db connected'))
   .catch((err) => console.log('db connection failed: ', err.message || err));
