121ad99d2e843a6bbc146af72dfa9ad9b80aeb4d
Teo <teowenlong0316@gmail.com>
2022-02-13 14:19:29 +0800

    added mongodb altas

--- db/index.js
+++ db/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/blog-backend')
+// mongoose.connect('mongodb://127.0.0.1:27017/blog-backend')
+mongoose.connect(process.env.MONGODB_URL)
   .then(() => console.log('db connected'))
   .catch((err) => console.log('db connection failed: ', err.message || err));
